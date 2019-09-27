0ea4ad57d2175559321e4ec96623a3657b6f3ad2
TharinduSK <tskumarage@gmail.com>
2019-09-27 08:26:30 -0700

    Network analysis completed: Degree distribution, clustering coefficient, centrality (degree, betweenness, closeness)

--- network_measure/network_analysis.py
+++ network_measure/network_analysis.py
@@ -8,30 +8,32 @@ Task : Friendship Network Analysis
 """
 
 import networkx as nx
-import random as rd
 import matplotlib.pyplot as plt
+import data_extraction.data_reader as dt
 
 
 def main():
     friendship_network = init_network()
 
-    node_list = list(range(1, 10))
-
-    edge_list = [(rd.randrange(1, 10), rd.randrange(10)) for x in range(1, 20)]
-    edge_list = list(dict.fromkeys(edge_list))
+    node_list, edge_list = dt.read_data()
 
     add_nodes(network=friendship_network, node_list=node_list)
 
-    print(friendship_network.nodes())
-
     add_edges(network=friendship_network, edge_list=edge_list)
 
-    print(friendship_network.edges())
+    print("Network Analysis of: ", friendship_network)
+    print()
 
-    print(friendship_network)
+    basic_network_report(network=friendship_network)  # print a basic analysis report on the network
 
-    visualize_network(network=friendship_network)
-    plot_degree_distribution(network=friendship_network)
+    plot_degree_distribution(network=friendship_network, plot_type='all')  # plot degree distribution
+
+    clustering_coefficient(network=friendship_network)  # plot and report local clustering coefficient
+
+    network_centrality(network=friendship_network)
+    # plot and report network centrality measures (degree, betweenness, closeness)
+
+
 
 
 def init_network():
@@ -54,19 +56,137 @@ def add_edges(network, edge_list):
 
 def visualize_network(network):
 
-    nx.draw_networkx(network)
+    nx.draw_random(network)
+
+    plt.savefig("graph.png", dpi=1000)
+
+
+def basic_network_report(network):
+    nodes = network.order()
+    edges = network.size()
+
+    in_degree_mat = network.in_degree()
+    average_in_degree = round(sum([x[1] for x in in_degree_mat])/nodes)
+    max_in_degree = max([x[1] for x in in_degree_mat])
+
+    out_degree_mat = network.out_degree()
+    average_out_degree = round(sum([x[1] for x in out_degree_mat]) / nodes)
+    max_out_degree = max([x[1] for x in out_degree_mat])
+
+    print("{:^65}".format("----------Basic Network Measures----------"))
+    print()
+    format_string = "{0:>40} : {1:<5}"
+    print(format_string.format("Number of Nodes in the Network", nodes))
+    print(format_string.format("Number of Edges in the Network", edges))
+    print(format_string.format("Average in-degree", average_in_degree))
+    print(format_string.format("Maximum in-degree", max_in_degree))
+    print(format_string.format("Average out-degree", average_out_degree))
+    print(format_string.format("Maximum out-degree", max_out_degree))
+    print(format_string.format("Number of strongly connected components", nx.number_strongly_connected_components(network)))
+    print(format_string.format("Number of weakly connected components", nx.number_weakly_connected_components(network)))
+
+
+def plot_degree_distribution(network, plot_type='all'):
+
+    if plot_type == 'in' or plot_type =='all':
+
+        in_degree_mat = network.in_degree()
+        in_degree_values = sorted([node[1] for node in in_degree_mat])
+        in_degrees = sorted(list(set(in_degree_values)))  # Get the unique in_degree values(bins)
+        in_degree_freq = [in_degree_values.count(i) for i in in_degrees]
+
+        if plot_type == 'in':
+            plt.title("In Degree Distribution")
+            plt.grid(True)
+            plt.ylabel("Frequency")
+            plt.xlabel("Degree")
+            plt.plot(in_degrees, in_degree_freq, 'bx-')
+            plt.hist(in_degree_values)
+
+            plt.show()
+
+    if plot_type == 'out' or plot_type =='all':
+        out_degree_mat = network.out_degree()
+        out_degree_values = sorted([node[1] for node in out_degree_mat])
+        out_degrees = sorted(list(set(out_degree_values))) # Get the unique in_degree values(bins)
+        out_degree_freq = [out_degree_values.count(i) for i in out_degrees]
+
+        if plot_type == 'out':
+            plt.title("Out Degree Distribution")
+            plt.grid(True)
+            plt.ylabel("Frequency")
+            plt.xlabel("Degree")
+
+            plt.plot(out_degrees, out_degree_freq, 'ro-')
+            plt.hist(out_degree_values)
+
+            plt.show()
+
+    if plot_type == 'all':
+        plt.title("Degree Distribution")
+        plt.grid(True)
+        plt.ylabel("Frequency")
+        plt.xlabel("Degree")
+        plt.legend(['In degree', 'Out degree'])
+        plt.plot(in_degrees, in_degree_freq, 'bx-')
+        plt.plot(out_degrees, out_degree_freq, 'ro-')
+
+        plt.show()
+
+
+def clustering_coefficient(network):
+
+    local_clustering_list = nx.clustering(network)
+    local_clustering_values = sorted([local_clustering_list[node] for node in local_clustering_list])
+
+    average_clustering = nx.average_clustering(network)
+
+    print()
+    print("{:^65}".format("----------Network Clustering Coefficient----------"))
+    print()
+    format_string = "{0:>40} : {1:<5.2f}"
+    print(format_string.format("Average Clustering Coefficient of the Network", average_clustering))
+
+    plt.title("Local Clustering Coefficient Distribution")
+    plt.grid(True)
+    plt.ylabel("Frequency")
+    plt.xlabel("Local Clustering Coefficient")
+    plt.hist(local_clustering_values)
 
     plt.show()
 
+def network_centrality(network):
 
-def plot_degree_distribution(network):
+    page_rank_list = nx.pagerank(network)
+    page_rank_values = sorted([page_rank_list[node] for node in page_rank_list])
 
-    degrees = [network.degree(i) for i in network.nodes()]
+    betweenness_list = nx.betweenness_centrality(network)
+    betweenness_values = sorted([betweenness_list[node] for node in betweenness_list])
 
-    plt.title("Degree Histogram")
-    plt.ylabel("Count")
-    plt.xlabel("Degree")
-    plt.hist(degrees)
+    closeness_list = nx.closeness_centrality(network)
+    closeness_values = sorted([closeness_list[node] for node in closeness_list])
+
+    plt.title("Degree Centrality Distribution (Pagerank)")
+    plt.grid(True)
+    plt.ylabel("Frequency")
+    plt.xlabel("Degree Centrality")
+    plt.hist(page_rank_values)
+
+    plt.show()
+
+    plt.title("Betweenness Centrality Distribution")
+    plt.grid(True)
+    plt.ylabel("Frequency")
+    plt.xlabel("Betweenness Centrality")
+    plt.hist(betweenness_values)
+
+    plt.show()
+
+    plt.title("Closeness Centrality Distribution")
+    plt.grid(True)
+    plt.ylabel("Frequency")
+    plt.xlabel("Closeness Centrality")
+    plt.hist(closeness_values)
 
     plt.show()
 
